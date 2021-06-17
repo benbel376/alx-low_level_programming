6d3f6828d13671c556ad5170f3744072739c274b
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-06-17 01:52:02 -0700

    commit 13

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *prints a certain out put using puts function 
+ * main -Entry point
+ * Return: Always 0 (Success)
 **/
+
 int main(void)
 {
-puts ("Programming is like building a multilingual puzzle");
-return(0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
