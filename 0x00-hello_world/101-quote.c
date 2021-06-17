0adf2d5b49713668f4738cd04872a70a265afaa6
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-06-17 10:36:20 -0700

    last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - outputs to string
  *
- * Return: Always 0 (Success)
+ * Description: Prints out "and that piece of art is useful..." without printf
+ * Return: 1
  */
 int main(void)
 {
-
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+asm ("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
+return (1);
 }
