843f6dc89de05c670671b2e632223c8604eaa614
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-07-05 06:58:42 -0700

    commit four

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,19 +11,16 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int c = 0;
-	int d = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		c = c + *(a + i * sizeof(int));
-	}
-
-	for (j = 0; j < size; j++)
-
-	{
-		d = d + *(a + (size * j) + (size - j - 1));
-	}
-	printf("%d, %d\n", c, d);
+int i, j;
+int c = 0;
+int d = 0;
+for (i = 0; i < size; i++)
+{
+c = c + *(*(a + i) + i);
+}
+for (j = 0; j < size; j++)
+{
+d = d + *(*(a + j) + (size -(1 + j)) );
+}
+printf("%d, %d\n", c, d);
 }
