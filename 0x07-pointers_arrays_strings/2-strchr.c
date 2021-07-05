0114575ea5c95e71b6f067d09f79006275ee095d
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-07-05 04:56:19 -0700

    commit four

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in string
+ * @s: string pointer
+ * @c: character
+ * Return: returns the charactger pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != c)
+{
+s++;
+}
+return (s);
+}
