e19972ac6f28664bc9d92dfa2ee4442e0a05149f
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-08-03 04:34:32 -0700

    commit three betty format correction

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,10 +2,15 @@
 #define LISTS_H
 
 int _putchar(char c);
+/**
+ * struct listint_t - singly list definition
+ * @n: data storage
+ * @next: pointer to next node
+ */
 typedef struct listint_t
 {
-  int n;
-  struct listint_t *next;
+int n;
+struct listint_t *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
