560ec19b5c26a9e42ef404163a25eef180972767
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-08-03 04:40:46 -0700

    commit five task two trial 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_listint - prints the content of the list
+ * @h: takes in a list pointer
+ *
+ * Return: size_t which s int
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t n = 0;
+while (h != NULL)
+{
+h = (*h).next;
+n = n + 1;
+}
+return (n);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-int _putchar(char c);
 /**
  * struct listint_t - singly list definition
  * @n: data storage
@@ -13,5 +12,7 @@ int n;
 struct listint_t *next;
 } listint_t;
 
+int _putchar(char c);
+size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 #endif
