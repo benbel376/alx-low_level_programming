a370cb55b8a88bc0fb426782df5be91fe69ed40c
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-08-03 04:28:32 -0700

    commit three betty format correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,19 +4,19 @@
 #include <stdio.h>
 /**
  * print_listint - prints the content of the list
- * @listint_t: takes in a list pointer
- * 
+ * @h: takes in a list pointer
+ *
  * Return: size_t which s int
  */
 
 size_t print_listint(const listint_t *h)
 {
-size_t n;
-while(h != NULL)
+size_t n = 0;
+while (h != NULL)
 {
 printf("%d\n", (*h).n);
 h = (*h).next;
-++n;
+n = n + 1;
 }
-return(n);
+return (n);
 }
